431c477637aedd2027e48af73aae00036f07a590
sriniselenium <srinibat@yahoo.com>
2024-08-01 21:07:15 -0400

    Added by new user

--- SampleGitProject/src/classwork/Car.java
+++ SampleGitProject/src/classwork/Car.java
@@ -7,6 +7,7 @@ public class Car {
 		System.out.println("In driver");
 		System.out.println("Added new line");
 		System.out.println("In branch 24.08");
+		System.out.println("Added by new automation engineer");
 	}
 
 }
